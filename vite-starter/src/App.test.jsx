4c64ddf39132856b67ccd94e2d6f06bf32e2d969
hyunjee <107829027+kimhyunjee@users.noreply.github.com>
2024-06-25 23:20:03 +0900

    feat: ffireEvent를 활용한 버튼 클릭 테스트

--- vite-starter/src/App.test.jsx
+++ vite-starter/src/App.test.jsx
@@ -1,13 +1,26 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
 
-test("버튼 색상 확인", () => {
+test("버튼 색상,라벨 확인", () => {
+  //render app
   render(<App />);
+
+  //find the button
   const buttonElement = screen.getByRole("button", { name: /blue/i });
+  // check initial color
   expect(buttonElement).toHaveClass("red");
 });
 
-test("버튼 텍스트 확인", () => {});
+test("클릭한 버튼 텍스트 확인", () => {
+  //click the button
+  fireEvent.click(buttonElement);
+});
 test("버튼 색상 확인", () => {});
-test("버튼 클릭 이후 색상 확인", () => {});
+test("버튼 클릭 이후 색상 확인", () => {
+  /red/i;
+});
+//check button text
+
+//  check  the button color
+expect(buttonElement).toHaveClass("blue");
 test("버튼 클릭 이후 텍스트 확인", () => {});
