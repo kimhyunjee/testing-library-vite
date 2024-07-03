4603db8e89ea9ac727894eb4fa78ddec28a20c28
hyunjee <107829027+kimhyunjee@users.noreply.github.com>
2024-07-03 23:42:05 +0900

    feat : 선데이 아이스크림 주문하기

---
+++ sundae-starter/src/pages/summary/test/SummaryForm.test.jsx
@@ -0,0 +1,27 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import SummaryForm from "../SummaryForm";
+
+test("최초 테스트", () => {
+  render(<SummaryForm />);
+  const checkbox = screen.getByRole("checkbox", {
+    name: /terms and conditions/i,
+  });
+  expect(checkbox).not.toBeChecked();
+
+  const confirmButton = screen.getByRole("button", { name: /confirm order/i });
+  expect(confirmButton).toBeDisabled();
+});
+
+test("버튼 클릭 시에 체크박스 활성화,두 번째 클릭 시에는 비활성화시키기 테스트", () => {
+  render(<SummaryForm />);
+  const checkbox = screen.getByRole("checkbox", {
+    name: /terms and conditions/i,
+  });
+  const confirmButton = screen.getByRole("button", { name: /confirm order/i });
+
+  fireEvent.click(checkbox);
+  expect(confirmButton).toBeEnabled();
+
+  fireEvent.click(checkbox);
+  expect(confirmButton).toBeDisabled();
+});
