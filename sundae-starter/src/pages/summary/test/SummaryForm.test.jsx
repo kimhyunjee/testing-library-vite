ed835e86f6e2701a50df33dee3404583d04c8d93
hyunjee <107829027+kimhyunjee@users.noreply.github.com>
2024-07-05 00:42:14 +0900

    feat : 마우스 호버 테스트 추가

--- sundae-starter/src/pages/summary/test/SummaryForm.test.jsx
+++ sundae-starter/src/pages/summary/test/SummaryForm.test.jsx
@@ -1,5 +1,6 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import SummaryForm from "../SummaryForm";
+import userEvent from "@testing-library/user-event";
 
 test("최초 테스트", () => {
   render(<SummaryForm />);
@@ -12,16 +13,39 @@ test("최초 테스트", () => {
   expect(confirmButton).toBeDisabled();
 });
 
-test("버튼 클릭 시에 체크박스 활성화,두 번째 클릭 시에는 비활성화시키기 테스트", () => {
+test("버튼 클릭 시에 체크박스 활성화,두 번째 클릭 시에는 비활성화시키기 테스트", async () => {
+  const user = userEvent.setup();
   render(<SummaryForm />);
   const checkbox = screen.getByRole("checkbox", {
     name: /terms and conditions/i,
   });
   const confirmButton = screen.getByRole("button", { name: /confirm order/i });
 
-  fireEvent.click(checkbox);
+  await user.click(checkbox);
   expect(confirmButton).toBeEnabled();
 
-  fireEvent.click(checkbox);
+  await user.click(checkbox);
   expect(confirmButton).toBeDisabled();
+});
+
+test("마우스 호버 시 popover되는지 테스트", async () => {
+  const user = userEvent.setup();
+
+  render(<SummaryForm />);
+  // 팝오버가 숨겨진 상태인지
+  const nullPopover = screen.queryByText(
+    /no ice cream will actually be delivered/i
+  );
+  expect(nullPopover).not.toBeInTheDocument();
+
+  // 체크박스 라벨에 마우스커서를 올렸을 때 표시돼야함
+  const termsAndConditions = screen.getByText(/terms and conditions/i);
+  await user.hover(termsAndConditions);
+  const popover = screen.getByText(/no ice cream will actually be delivered/i);
+
+  expect(popover).toBeInTheDocument();
+
+  // 커서를 이동하면 사라져야함
+  await user.unhover(termsAndConditions);
+  expect(popover).not.toBeInTheDocument();
 });
