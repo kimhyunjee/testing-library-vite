a07c33e56d39f16dbdb149b1000b47f8a9371402
hyunjee <107829027+kimhyunjee@users.noreply.github.com>
2024-07-04 21:11:34 +0900

    feat : form order 추가

--- sundae-starter/src/pages/summary/SummaryForm.jsx
+++ sundae-starter/src/pages/summary/SummaryForm.jsx
@@ -1,3 +1,29 @@
+import { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
 export default function SummaryForm() {
-  return <div></div>;
+  const [isChecked, setIsChecked] = useState(false);
+  const checkboxLabel = (
+    <span>
+      <span style={{ color: "blue" }}>terms and conditions</span>
+    </span>
+  );
+  return (
+    <div>
+      <Form>
+        <Form.Group controlId="terms-and-conditions">
+          <Form.Check
+            type="checkbox"
+            checked={isChecked}
+            onChange={(e) => setIsChecked(e.target.checked)}
+            label={checkboxLabel}
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit" disabled={!isChecked}>
+          Confirm order
+        </Button>
+      </Form>
+    </div>
+  );
 }
