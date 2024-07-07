c40112e15da7a0f8cf6c98f0e7f8d11c503d2097
hyunjee <107829027+kimhyunjee@users.noreply.github.com>
2024-07-07 23:19:51 +0900

    feat: react코드 수정

--- sundae-starter/src/pages/summary/SummaryForm.jsx
+++ sundae-starter/src/pages/summary/SummaryForm.jsx
@@ -1,12 +1,28 @@
 import { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import Popover from "react-bootstrap/Popover";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
 
 export default function SummaryForm() {
   const [isChecked, setIsChecked] = useState(false);
+
+  // function handleSubmit(event) {
+  //   event.preventDefault();
+  //   setOrderPhase("completed");
+  // }
+
+  const popover = (
+    <Popover id="popover-basic">
+      <Popover.Body>아이스크림이 실제로 배달되진 않습니다</Popover.Body>
+    </Popover>
+  );
+
   const checkboxLabel = (
     <span>
-      <span style={{ color: "blue" }}>terms and conditions</span>
+      <OverlayTrigger placement="right" overlay={popover}>
+        <span style={{ color: "blue" }}>terms and conditions</span>
+      </OverlayTrigger>
     </span>
   );
   return (
