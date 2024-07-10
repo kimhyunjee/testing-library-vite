a8200da26bc5791d3f58b1756d41f75d162bfbbd
hyunjee <107829027+kimhyunjee@users.noreply.github.com>
2024-07-10 20:55:17 +0900

    feat : MSW 설정

--- sundae-starter/src/setupTests.js
+++ sundae-starter/src/setupTests.js
@@ -1,1 +1,11 @@
 import "@testing-library/jest-dom";
+
+import { beforeAll, afterEach, afterAll } from "vitest";
+import { server } from "./mocks/server";
+
+// 모든 테스트 전에 api mocking
+beforeAll(() => server.listen());
+// 테스트 후에 핸들러 리셋
+afterEach(() => server.resetHandlers());
+// 모든 테스트 후 서버 종료
+afterAll(() => server.close());
