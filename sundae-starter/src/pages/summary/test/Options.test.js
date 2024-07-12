abef6ae994c20206d738f96283cce0f4dd136660
hyunjee <107829027+kimhyunjee@users.noreply.github.com>
2024-07-12 23:34:33 +0900

    test : scoop image 테스트코드 추가

---
+++ sundae-starter/src/pages/summary/test/Options.test.js
@@ -0,0 +1,14 @@
+import { render, screen } from "@testing-library/react";
+import Options from "../Options";
+
+test("서버에서 반환한 scoop가 종류별로 이미지가 나오는지", async () => {
+  render(<Options optionType="scoops" />);
+
+  // 이미지 찾기
+  const scoopImages = screen.getAllByRole("img", { name: /scoop$/i });
+  expect(scoopImages).toHaveLength(2);
+
+  //  alt text 확인
+  const altText = scoopImages.map((element) => element.alt);
+  expect(altText).toEqual(["Chocolate scoop", "Vanilla scoop"]);
+});
